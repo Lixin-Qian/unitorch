08cea2bd6a5b269489cbbc6afa55adbbc2cf4581
fuliucansheng <fuliucansheng@gmail.com>
2022-03-14 00:40:55 +0800

    fix workflow

--- tests/models/test_vlp.py
+++ tests/models/test_vlp.py
@@ -5,8 +5,14 @@ import unitorch
 import torch
 from PIL import Image
 from absl.testing import absltest, parameterized
+<<<<<<< HEAD
 from unitorch import set_seed, hf_cached_path
 from unitorch.models.vlp import VLPForGeneration, VLPProcessor
+=======
+from unitorch import set_seed
+from unitorch.models.vlp import VLPForGeneration, VLPProcessor
+from unitorch.cli import cached_path
+>>>>>>> f16553e (add test vlp)
 from unitorch.cli import CoreConfigureParser
 from unitorch.cli.models.vlp.modeling import VLPForGeneration as CLI_VLPForGeneration
 from unitorch.cli.models.vlp.processing import VLPProcessor as CLI_VLPProcessor
@@ -17,6 +23,7 @@ class VLPTest(parameterized.TestCase):
     def setUp(self):
         set_seed(42)
 
+<<<<<<< HEAD
         config_path = pkg_resources.resource_filename(
             "unitorch", "examples/configs/core/generation/vlp.ini"
         )
@@ -37,6 +44,24 @@ class VLPTest(parameterized.TestCase):
         self.vocab_path = hf_cached_path(
             "https://huggingface.co/microsoft/unilm-base-cased/resolve/main/vocab.txt"
         )
+=======
+        config_path = pkg_resources.resource_filename("unitorch", "examples/configs/core/generation/vlp.ini")
+        self.config = CoreConfigureParser(config_path)
+
+        self.detection_config_path = cached_path(
+            "https://huggingface.co/fuliucansheng/detectron2/resolve/main/vlp/faster_rcnn_x101_64x4d_fpn_2x_config.yaml"
+        )
+        self.detection_weight_path = cached_path(
+            "https://huggingface.co/fuliucansheng/detectron2/resolve/main/vlp/faster_rcnn_x101_64x4d_fpn_2x_model.bin"
+        )
+        self.vlp_config_path = cached_path(
+            "https://huggingface.co/fuliucansheng/detectron2/resolve/main/vlp/config.json"
+        )
+        self.vlp_weight_path = cached_path(
+            "https://huggingface.co/fuliucansheng/detectron2/resolve/main/vlp/pytorch_model.bin"
+        )
+        self.vocab_path = cached_path("https://huggingface.co/fuliucansheng/detectron2/resolve/main/vlp/vocab.txt")
+>>>>>>> f16553e (add test vlp)
 
     @parameterized.named_parameters(
         {
@@ -54,7 +79,11 @@ class VLPTest(parameterized.TestCase):
         decode,
         max_gen_seq_length=10,
     ):
+<<<<<<< HEAD
         image = hf_cached_path(image)
+=======
+        image = cached_path(image)
+>>>>>>> f16553e (add test vlp)
         model = CLI_VLPForGeneration.from_core_configure(self.config)
         model.eval()
         process = CLI_VLPProcessor.from_core_configure(self.config)
@@ -83,7 +112,11 @@ class VLPTest(parameterized.TestCase):
         decode,
         max_gen_seq_length=10,
     ):
+<<<<<<< HEAD
         image = hf_cached_path(image)
+=======
+        image = cached_path(image)
+>>>>>>> f16553e (add test vlp)
         model = VLPForGeneration(self.vlp_config_path, self.detection_config_path)
         model.vision_model.from_pretrained(self.detection_weight_path)
         model.from_pretrained(self.vlp_weight_path)
